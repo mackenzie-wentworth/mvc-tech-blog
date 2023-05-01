aeb95962cd4eb6343f1e1bf59cc4d0ccdc9fb16e
mackenzie-wentworth <mwentworth28@gmail.com>
2023-05-01 12:31:59 -0400

    added content to 'index.js' under 'models' folder

--- models/index.js
+++ models/index.js
@@ -3,5 +3,34 @@ const User = require('./User');
 const BlogPost = require('./BlogPost');
 const Comment = require('./Comment');
 
+
+// BlogPost belongs to one User
+BlogPost.belongsTo(User, {
+    foreignKey: 'user_id',
+
+})
+// User can have many BlogPosts
+User.hasMany(BlogPost, {
+    foreignKey: 'user_id',
+})
+
+// BlogPost can have many Comments
+// When we delete a BlogPost, make sure to delete the associated Comments
+BlogPost.hasMany(Comment, {
+    foreignKey: 'blogPost_id',
+    onDelete: 'CASCADE',
+})
+
+// A Comment belongs to one User
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+})
+
+// A User can have many Comments
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+})
+
+
 // TODO: Export models as an object
 module.exports = { User, BlogPost, Comment };
