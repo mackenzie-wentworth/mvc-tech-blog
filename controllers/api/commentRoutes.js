5f24a2bac3a4ed6f5d6de1223176dbc96339bb76
mackenzie-wentworth <mwentworth28@gmail.com>
2023-05-01 14:05:56 -0400

    added 'post' method to CREATE comment on 'commentRoutes.js' file under api folder

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,12 +2,21 @@
 
 // TODO: Import dependencies, including express 'router' and withAuth
 const router = require("express").Router();
-const { User, BlogPost, Comment } = require("../../models/");
+const { Comment } = require("../../models/");
 const withAuth = require("../../utils/auth");
 
 // TODO: Use express-session to store session data in a cookie
 
 // TODO: Comment on a blog post (POST method with 'create')
+router.post("/", withAuth, (req, res) => {
+    Comment.create({ ...req.body, user_id: req.session.user_id })
+        .then(commentData => {
+            res.json(commentData);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
 
 // TODO: Export 'router'
 module.exports = router;
