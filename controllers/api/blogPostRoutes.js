756a46aece1cc9fafe8267de98d6eb686d41fcdb
mackenzie-wentworth <mwentworth28@gmail.com>
2023-05-01 15:22:59 -0400

    added option to CREATE, UPDATE, DELETE blog post on 'blogPostRoutes.js' file under api folder

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -2,16 +2,67 @@
 
 // TODO: Import dependencies, including express 'router' and withAuth
 const router = require("express").Router();
-const { User, BlogPost, Comment } = require("../../models/");
+const { BlogPost } = require("../../models/");
 const withAuth = require("../../utils/auth");
 
 // TODO: Use express-session to store session data in a cookie
 
-// TODO: Create new blog post (POST method with 'create')
+// TODO: CREATE new blog post (POST method with 'create')
+router.post('/', withAuth, (req, res) => {
+    BlogPost.create({
+        title: req.body.title,
+        contents: req.body.contents,
+        date_created: req.body.date_created,
+        user_id: req.session.user_id
+    })
+        .then(blogPostData => res.json(blogPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
-// TODO: Update existing blog post (PUT method with 'update')
-
-// TODO: Delete blog post (DELETE method with 'destroy')
+// TODO: UPDATE existing blog post (PUT method with 'update')
+router.put('/:id', withAuth, (req, res) => {
+    // Update multiple instances that match the where options
+    BlogPost.update({
+        title: req.body.title,
+        contents: req.body.contents
+    },
+        {
+            where: {
+                id: req.params.id
+            }
+        }).then(blogPostData => {
+            if (!blogPostData) {
+                res.status(404).json({ message: 'Sorry, no blog post found with this id!' });
+                return;
+            }
+            res.json(blogPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+// TODO: DELETE blog post (DELETE method with 'destroy')
+router.delete('/:id', withAuth, (req, res) => {
+    // Delete multiple instances, in this case just where the id has been selected. On a post/:id page, clicking the delete button will trigger the front end form that will ship a delete request back here
+    BlogPost.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(blogPostData => {
+        if (!blogPostData) {
+            res.status(404).json({ message: 'Sorry, no blog post found with this id!' });
+            return;
+        }
+        res.json(blogPostData);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 // TODO: Export 'router'
 module.exports = router;
