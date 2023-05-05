a37d41f174343a57fe4261b522946c0bee4caabb
mackenzie-wentworth <mwentworth28@gmail.com>
2023-05-05 12:41:40 -0400

    set up 'logout.js' file under public/js folder

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,1 +1,16 @@
 // TODO: Create "document" querySelector and addEventListener for when user clicks "logout" option
+const logout = async () => {
+    const response = await fetch('/api/users/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    // Redirects to homepage (homeRoutes) once user clicks logout option
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  };
+  
+  document.querySelector('#logout').addEventListener('click', logout);
