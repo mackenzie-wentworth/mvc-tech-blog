d84744de22e16ae219d92189a25bab9ab9042044
mackenzie-wentworth <mwentworth28@gmail.com>
2023-05-05 12:38:25 -0400

    added 'email' property to USER model; added 'dashboardRoutes' to api/index.js file; set up 'login.js' file under public/js folder

--- public/js/login.js
+++ public/js/login.js
@@ -1,29 +1,65 @@
 // TODO: Set up async event for 'loginFormHandler'
+const loginFormHandler = async (event) => {
+    event.preventDefault();
 
-// TODO: Set up querySelectors for login form
+    // TODO: Set up querySelectors for login form
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
 
-// TODO: Send POST request to API endpoint
+    // TODO: Send POST request to API endpoint
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
 
-// TODO: Redirect the browser to the user's view of user's dashboard page (if user login is successful)
-
-
+        if (response.ok) {
+            // TODO: Redirect the browser to the user's view of user's dashboard page (if user login is successful)
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
 
 
 
 
 // TODO: Set up async event for 'signupFormHandler'
+const signupFormHandler = async (event) => {
+    event.preventDefault();
 
-// TODO: Set up querySelectors for signup form
+    // TODO: Set up querySelectors for signup form
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
 
-// TODO: Send POST request to API endpoint
+    // TODO: Send POST request to API endpoint
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ username, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
 
-// TODO: Redirect the browser to the user's view of user's dashboard page (if user signup is successful)
-
-
-
+        if (response.ok) {
+            // TODO: Redirect the browser to the user's view of user's dashboard page (if user signup is successful)
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
 
 
 
 // TODO: Create "document" querySelector and addEventListener for loginFormHandler (through submit button)
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
 
 // TODO: Create "document" querySelector and addEventListener for signupFormHandler (through submit button)
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
