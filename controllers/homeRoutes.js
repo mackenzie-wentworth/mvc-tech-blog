a49060166c7969c76aba60b804fc9ba3438549c0
mackenzie-wentworth <mwentworth28@gmail.com>
2023-05-07 12:08:43 -0400

    added 'GET' method to homeRoutes.js under api folder to get blogPost by :id

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,5 @@
 // HOMEPAGE (displays existing blogposts, does NOT require user to be logged in)
+// TODO: Use express-session to store session data in a cookie
 
 const router = require('express').Router();
 const { User, BlogPost, Comment } = require('../models');
@@ -12,8 +13,8 @@ const withAuth = require('../utils/auth');
 //     }
 //   });
 
-
-// GET all Blogposts to view on homepage (does NOT require withAuth)
+// TODO: If the user is logged in, allow them to view all existing blog posts from all users
+// GET all BlogPosts to view on homepage (does NOT require withAuth)
 router.get('/', async (req, res) => {
   try {
     // Get all projects and JOIN with user data
@@ -28,18 +29,47 @@ router.get('/', async (req, res) => {
 
     const blogPosts = blogPostData.map((blogPost) => blogPost.get({ plain: true }));
 
-    res.render('homepage', { 
-      blogPosts, 
-      logged_in: req.session.logged_in 
+    res.render('homepage', {
+      blogPosts,
+      logged_in: req.session.logged_in
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// TODO: Use express-session to store session data in a cookie
+// TODO: Option for user to click on a specific existing blog post (so the user can add a comment)
+// Get one BlogPost, be sure to include it's associated User and Comments
+router.get('/blogPost/:id', async (req, res) => {
+  try {
+    const blogPostData = await BlogPost.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+        {
+          model: Comment,
+          attributes: ['comment_body', 'date_created'],
+          include: [
+            {
+              model: User,
+              attributes: ['username']
+            }
+          ]
+        }
+      ],
+    });
 
-// Get one blog post, be sure to include it's associated User and Comments
+    const blogPost = blogPostData.get({ plain: true });
 
+    res.render('blogPost', {
+      ...blogPost,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
