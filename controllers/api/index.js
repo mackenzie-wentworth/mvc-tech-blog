5726f8f121f172edc5690447f48b7c3136d26520
mackenzie-wentworth <mwentworth28@gmail.com>
2023-05-16 16:13:30 -0400

    created a 'commentRoutes.js' file under controllers/api folder

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,11 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const blogPostRoutes = require('./blogPostRoutes');
-// const commentRoutes = require('./commentRoutes');
+const commentRoutes = require('./commentRoutes');
 
 router.use('/users', userRoutes);
 router.use('/blogPosts', blogPostRoutes);
-// router.use('/comments', commentRoutes);
+router.use('/comments', commentRoutes);
 
 // TODO: Export 'router'
 module.exports = router;
